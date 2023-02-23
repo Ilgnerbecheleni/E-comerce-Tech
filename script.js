9fa40bf197ab003135eb2dc7a5d9fff873f3b51f
ilgner becheleni <ilgner.gui@gmail.com>
2023-02-23 00:57:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.buy');
+const cart = document.querySelector('.cart');
+const msg = document.querySelector('.msg')
+let cart_quant = 0;
+
+$('.buy').click(function() {
+    $('#mymodal').modal('show');
+    cart.classList.remove('hidden')
+    cart_quant++;
+    cart.setAttribute('cart-valor', cart_quant);
+  });
+
+
+console.log(buttons);
+
+
+
+
+
